c3ecf7460681eee2630664363fe569c1e86eb441
root <example@gmail.com>
2026-09-21 02:22:14 +0000

    Temporary diagnostic: check full-screen intent permission

--- rudolf-driver-android/android/app/src/main/java/com/rudolfride/driver/MainActivity.java
+++ rudolf-driver-android/android/app/src/main/java/com/rudolfride/driver/MainActivity.java
@@ -1,5 +1,34 @@
 package com.rudolfride.driver;
 
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        boolean allowed = true;
+
+        if (Build.VERSION.SDK_INT >= 34) {
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+
+            allowed =
+                    notificationManager != null &&
+                    notificationManager.canUseFullScreenIntent();
+        }
+
+        Toast.makeText(
+                this,
+                "Full-screen intent allowed: " +
+                        (allowed ? "YES" : "NO"),
+                Toast.LENGTH_LONG
+        ).show();
+    }
+}
